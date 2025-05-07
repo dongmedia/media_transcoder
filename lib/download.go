26ecff48ccb936db1e33b31a4ed47d60102db62c
Dong H. Kim <ehdgus1524@gmail.com>
2025-05-07 13:03:11 +0900

    changed: downloadvideo

--- lib/download.go
+++ lib/download.go
@@ -30,88 +30,9 @@ func Download(ctx context.Context, url, fileName, gpuType string) error {
 	return nil
 }
 
-func DownloadLink(ctx context.Context, url, fileName string) error {
-	if ctx.Err() != nil {
-		return ctx.Err()
-	}
-
-	// fileFormat := filepath.Ext(fileName)
-
-	url = strings.Trim(url, " ")
-
-	ffmpegPath := os.Getenv("FFMPEG_PATH")
-	if ffmpegPath == "" {
-		ffmpegPath = "ffmpeg" // 기본값
-	}
-
-	cmd := exec.CommandContext(ctx, ffmpegPath,
-		"-i", url,
-		"-c:v", "libx264",
-		"-c:a", "copy",
-		fileName,
-	)
-
-	// FFmpeg 명령 로깅
-	log.Printf("Transcode Stream into Video: %v", fileName)
-
-	// 명령 실행 및 오류 처리
-	output, err := cmd.CombinedOutput()
-
-	if err != nil {
-
-		log.Printf("Transcoding Error (Job %s): %v\n%s", url, err, string(output))
-
-		return err
-	}
-	log.Printf("Finished: %v", fileName)
-
-	return nil
-}
-
-func DownloadHlsToVideo(ctx context.Context, url, fileName string) error {
-	if ctx.Err() != nil {
-		return ctx.Err()
-	}
-
-	url = strings.Trim(url, " ")
-	// fileFormat := filepath.Ext(fileName)
-
-	ffmpegPath := os.Getenv("FFMPEG_PATH")
-	if ffmpegPath == "" {
-		ffmpegPath = "ffmpeg" // 기본값
-	}
-
-	cmd := exec.CommandContext(ctx, ffmpegPath,
-		"-i", url,
-		"-c:v", "libx264",
-		"-c:a", "copy",
-		fileName,
-	)
-
-	// FFmpeg 명령 로깅
-	log.Printf("Transcode HLS Stream into Video: %v", fileName)
-
-	// 명령 실행 및 오류 처리
-	output, err := cmd.CombinedOutput()
-
-	if err != nil {
-		log.Printf("Transcoding Error (Job %s): %v\n%s", url, err, string(output))
-
-		return err
-	}
-
-	log.Printf("Finished: %v", fileName)
-
-	return nil
-}
-
 func DownloadHlsViaGpuVideo(ctx context.Context, url, fileName, gpuType string) error {
 	if ctx.Err() != nil {
 		return ctx.Err()
-	}
-
-	if gpuType == "" {
-		gpuType = "apple"
 	}
 
 	url = strings.Trim(url, " ")
